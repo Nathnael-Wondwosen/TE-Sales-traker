0dfa4052764e108f438f04c958201cfa7b04ac0b
IT2 <testnati95@gmail.com>
2025-10-27 17:33:09 +0300

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'default-no-store';
 import { redirect } from 'next/navigation';
 import { auth } from '@/lib/auth';
 import { getDashboardPath } from '@/lib/rbac';
